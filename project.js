538d9ba21bfc40681ea5576697fbd45655555080
Nate Allen <neight.allen@gmail.com>
2016-01-06 12:08:48 -0700

    Moved evaluated function to its own function

--- project.js
+++ project.js
@@ -3,17 +3,20 @@ var prettyjson = require('prettyjson');
 
 Nightmare()
   .goto('https://www.kickstarter.com/projects/728836843/codrone-learn-to-code-with-programmable-drone/description')
-  .evaluate(function (){
-    backers = $('#backers_count').data('backers-count');
-    pledged = $('#pledged').data('pledged');
-    goal = $('#pledged').data('goal');
-    return {
-      backers: backers,
-      goal: goal,
-      pledged: pledged
-    };
-  })
+  .evaluate(extractData)
   .then(function(result) {
-      console.log(prettyjson.render(data));
+      console.log(prettyjson.render(result));
     })
   .end()
+
+function extractData (){
+  backers = $('#backers_count').data('backers-count');
+  pledged = $('#pledged').data('pledged');
+  goal = $('#pledged').data('goal');
+  
+  return {
+    backers: backers,
+    goal: goal,
+    pledged: pledged
+  };
+}

0c0e1875ed76b5aac1f8e7832d7320376d59c3ea
Nate Allen <neight.allen@gmail.com>
2016-01-06 12:03:53 -0700

    Extracting a few pieces of info

--- project.js
+++ project.js
@@ -3,7 +3,14 @@ var Nightmare = require('nightmare');
 Nightmare()
   .goto('https://www.kickstarter.com/projects/728836843/codrone-learn-to-code-with-programmable-drone/description')
   .evaluate(function (){
-    return $('title:first').text();
+    backers = $('#backers_count').data('backers-count');
+    pledged = $('#pledged').data('pledged');
+    goal = $('#pledged').data('goal');
+    return {
+      backers: backers,
+      goal: goal,
+      pledged: pledged
+    };
   })
   .then(function(result) {
       console.log(result);

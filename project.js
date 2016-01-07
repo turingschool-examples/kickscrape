ceec2cbd87057da97abd0b566c5755f3e632f533
Nate Allen <neight.allen@gmail.com>
2016-01-06 17:26:25 -0700

    Not working, but need to move on

--- project.js
+++ project.js
@@ -3,6 +3,8 @@ var prettyjson = require('prettyjson');
 
 Nightmare()
   .goto('https://www.kickstarter.com/projects/728836843/codrone-learn-to-code-with-programmable-drone/description')
+  .click('.js-load-project-content')
+  .wait('.js-project-rewards')
   .evaluate(extractData)
   .then(function(result) {
       console.log(prettyjson.render(result));
@@ -13,10 +15,22 @@ function extractData (){
   backers = $('#backers_count').data('backers-count');
   pledged = $('#pledged').data('pledged');
   goal = $('#pledged').data('goal');
+
+  rewards = [];
+
+  $('.js-project-rewards li').each(function(i, $reward){
+    
+    reward_data = {
+      pledge: $reward.find(".pledge__currency-conversion").text(),
+      description: $reward.find(".pledge__reward-description pledge__reward-description--expanded").text()
+    }
+    rewards.push(reward_data);
+  })
   
   return {
     backers: backers,
     goal: goal,
-    pledged: pledged
+    pledged: pledged,
+    rewards: rewards
   };
 }
